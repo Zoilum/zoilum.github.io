88b29f5a04cd98e8d19d2ceb4e44d2551f81211f
Thomas Sommavilla <thomsomma@gmail.com>
2024-04-05 01:52:18 +0200

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@nuxt/image"],
+  image: {
+    domains: ["https://raw.githubusercontent.com"],
+  },
 });
